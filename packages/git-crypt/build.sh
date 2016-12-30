6b78358c3d033a89e0f9647550eac802a3bda60e
Felix Bartels <felix@host-consultants.de>
2016-12-30 11:57:03 +0100

    add first steps to build git-crypt

---
+++ packages/git-crypt/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=https://www.agwa.name/projects/git-crypt/
+TERMUX_PKG_DESCRIPTION="Transparent file encryption in git"
+TERMUX_PKG_VERSION=0.5.0
+TERMUX_PKG_SRCURL=https://www.agwa.name/projects/git-crypt/downloads/git-crypt-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_FOLDERNAME=git-crypt-${TERMUX_PKG_VERSION}
+TERMUX_PKG_BUILD_IN_SRC=yes
+TERMUX_PKG_DEPENDS="openssl"
